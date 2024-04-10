2d8dc9b221a6cbedfbeffd8b45fa22ab8dcd7ad7
sensival <alstjs883@naver.com>
2024-04-10 11:07:51 +0900

    행렬/배열

--- 07ch.R
+++ 07ch.R
@@ -29,10 +29,22 @@ test2
 # matrix연산 ####
 mat1<-matrix(1:9, nrow = 3)
 mat2<-matrix(c(10,20,30,40,50,60,70,80,90), nrow = 3)
-mat1+10
+mat1+10 #대입은 아님
 mat1+mat2 # 크기다르면 연산 x
-
+colSums(mat1)
+rowSums(mat1)
 
 # 인덱싱 ####
+a<-matrix(1:12, nrow=3)
+a<-a[,-c(1,3)] # 열삭제
 
 
+# 필터링 ####
+a[a%%2==0, drop=F]
+mat1<-matrix(1:6, ncol = 3, byrow=T)
+mat1[mat1[,3]>3,,drop=F]#3열의 값이 3보다 큰 걸 가지고 있는 행 필터링
+mat1[mat1[,2]==2,,drop=F]#2열의 값이 2인걸 가지고 있는 행 필터링
+
+# 배열####
+a <- array(1:12, dim=c(3,2,2))
+a
