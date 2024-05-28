cc752c7a7f8b9dd16a3500fa8a1c6a5edb3cf52d
sensival <alstjs883@naver.com>
2024-05-28 18:27:55 +0900

    시각화 연습

--- 13ch.R
+++ 13ch.R
@@ -10,11 +10,10 @@ if(!require(ggplot2)) install.packages("ggplot2")
 if(!require(dplyr)) install.packages("dplyr"); library(dplyr)
 if(!require(ggplot2)) install.packages("ggplot2"); library(ggplot2)
 str(Orange)
+# ggplot(aes(x=c(1:5), y=(1:5)))+geom_point(color = 'red') 이런건 안됨. dataframe에서 나와야함
 Orange %>% filter(Tree==1) %>% ggplot(aes(age, circumference))+geom_line()
-Orange %>% filter(Tree==1) %>% ggplot(aes(age, circumference))+geom_line(color = 'red')
-
-
-ggplot(Orange, aes(age, circumference, color = Tree)) + geom_line()
+Orange %>% filter(Tree==1) %>% ggplot(aes(age, circumference))+geom_line(colour = 'red')
+ggplot(Orange, aes(age, circumference, colour = Tree)) + geom_line()
 ggplot(Orange, aes(age, circumference)) + geom_line(aes(color = Tree))
 
 
@@ -26,7 +25,7 @@ mpg <-as.data.frame(ggplot2::mpg)
 class_counts <- as.data.frame(table(mpg$class))
 colnames(class_counts) <- c("class", "count")
 ggplot(data=class_counts, aes(x=class, y=count, fill=class)) +geom_bar(stat="identity", color="black")
-
+ggplot(data=class_counts, aes(x=class, y=count, fill=class)) +geom_bar(stat="identity", colour="black")
 
 # 막대그래프의 class변수 수에 따라 막대 테두리색이 여러개로 지정
 ggplot(data=mpg, aes(x=class, color=class)) + geom_bar() 
@@ -45,3 +44,35 @@ p <- ggplot(ToothGrowth, aes(x=dose, y=len)) + geom_boxplot()
 p
 #  notch = True 인자를 통해서, box plot 모양 변경
 ggplot(ToothGrowth, aes(x=dose, y=len)) + geom_boxplot(notch=TRUE)
+# ggplot2 패키지 로드
+library(ggplot2)
+
+# iris 데이터셋을 사용하여 박스 그래프 그리기
+ggplot(data = iris, aes(x = Species, y = Sepal.Length)) +
+  geom_boxplot() +
+  labs(title = "Boxplot of Sepal Length by Species",
+       x = "Species",
+       y = "Sepal Length") +
+  theme_minimal()
+
+
+ggplot(data=iris, aes(x=Species, y=Sepal.Length))+theme_light()+geom_boxplot(aes(fill=Species), outlier.shape=21)
+
+
+# Orange ####
+ggplot(Orange, aes(x=age, y=circumference, fill=Tree))+geom_bar(stat='identity')
+ggplot(Orange, aes(x=age, y=circumference))+geom_line(size=1, aes(color=Tree))
+ggplot(Orange, aes(x=age, y=circumference, color=Tree))+geom_point()
+ggplot(Orange, aes(x=age, y=circumference, color=Tree))+geom_line(size=4)
+
+
+# Cars93 ####
+if(!require(dplyr)) install.packages("dplyr"); library(dplyr)
+if(!require(ggplot2)) install.packages('ggplot2'); library(ggplot2)
+if(!require(MASS)) install.packages("MASS"); library(MASS)
+data('Cars93')
+
+#내부에색상을지정한히스토그램을그리며, x축에탑승자수, 탑승자수에따른분포를출력함
+ggplot( Cars93, aes(x=Passengers)) +  
+geom_histogram(fill="#F8766D")   
+                                        #ggsave("histogram_col.jpg", dpi = 300)
